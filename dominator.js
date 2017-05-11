3aa45215ae4899cd912d20fc03da6527d5cb15a6
Derek Johnson <drk.w.jhnsn@gmail.com>
2017-05-11 17:37:19 -0500

    implement dominator

--- dominator.js
+++ dominator.js
@@ -2,15 +2,19 @@ const { flattenTreeToArray } = require('./dom-util');
 const _ = require('underscore');
 
 const getElementById = function(root, id) {
-  // Your code here
+  const $ = flattenTreeToArray(root);
+  const ids = _.pluck($, 'id');
+  return $[ids.indexOf(id)];
 };
 
 const getElementsByClassName = function(root, className) {
-  // Your code here
+  const $ = flattenTreeToArray(root);
+  return $.filter(item => _.contains(item.classList, className));
 };
 
 const getElementsByTagName = function(root, tagName) {
-  // Your code here
+  const $ = flattenTreeToArray(root);
+  return $.filter(item => item.tagName === tagName);
 };
 
 module.exports = {
